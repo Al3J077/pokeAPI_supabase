b86614a142c670fa25cb986d54374e71b2e7470b
Al3J077 <garciaalejo711@gmail.com>
2025-05-15 21:20:48 +0000

    errores

--- proyecto/src/supabase.js
+++
@@ -1,6 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://bnuvwhuycdirdfcuzizu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJudXZ3aHV5Y2RpcmRmY3V6aXp1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzMzOTE0MCwiZXhwIjoyMDYyOTE1MTQwfQ.-6juShTDOtvrQ0Q8EwnVG3KTmCaAjE9IUZ-xfFC_4Us';
-export const supabase = createClient(supabaseUrl, supabaseKey);
